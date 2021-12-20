3e3dce4181cc9732ea743100738597ca241bb4fe
Guillaume Piolat <contact@auburnsounds.com>
2021-12-20 15:03:59 +0100

    Less templates.

--- source/wren/opcodes.d
+++ source/wren/opcodes.d
@@ -218,23 +218,9 @@ static immutable WREN_OPCODE_FULL_TABLE =
     WrenOpcode("END", 0),
 ];
 
-// Generate the enum table for opcodes
-mixin (() {
-    string str = "enum Code {";
-    static foreach(op; WREN_OPCODE_FULL_TABLE) {
-        str ~= "CODE_" ~ op.name ~ ", ";
-    }
-    str ~= "}";
-    return str;
-}());
+// Enum table for opcodes
+enum Code {CODE_CONSTANT, CODE_NULL, CODE_FALSE, CODE_TRUE, CODE_LOAD_LOCAL_0, CODE_LOAD_LOCAL_1, CODE_LOAD_LOCAL_2, CODE_LOAD_LOCAL_3, CODE_LOAD_LOCAL_4, CODE_LOAD_LOCAL_5, CODE_LOAD_LOCAL_6, CODE_LOAD_LOCAL_7, CODE_LOAD_LOCAL_8, CODE_LOAD_LOCAL, CODE_STORE_LOCAL, CODE_LOAD_UPVALUE, CODE_STORE_UPVALUE, CODE_LOAD_MODULE_VAR, CODE_STORE_MODULE_VAR, CODE_LOAD_FIELD_THIS, CODE_STORE_FIELD_THIS, CODE_LOAD_FIELD, CODE_STORE_FIELD, CODE_POP, CODE_CALL_0, CODE_CALL_1, CODE_CALL_2, CODE_CALL_3, CODE_CALL_4, CODE_CALL_5, CODE_CALL_6, CODE_CALL_7, CODE_CALL_8, CODE_CALL_9, CODE_CALL_10, CODE_CALL_11, CODE_CALL_12, CODE_CALL_13, CODE_CALL_14, CODE_CALL_15, CODE_CALL_16, CODE_SUPER_0, CODE_SUPER_1, CODE_SUPER_2, CODE_SUPER_3, CODE_SUPER_4, CODE_SUPER_5, CODE_SUPER_6, CODE_SUPER_7, CODE_SUPER_8, CODE_SUPER_9, CODE_SUPER_10, CODE_SUPER_11, CODE_SUPER_12, CODE_SUPER_13, CODE_SUPER_14, CODE_SUPER_15, CODE_SUPER_16, CODE_JUMP, CODE_LOOP, CODE_JUMP_IF, CODE_AND, CODE_OR, CODE_CLOSE_UPVALUE, CODE_RETURN, CODE_CLOSURE, CODE_CONSTRUCT, CODE_FOREIGN_CONSTRUCT, CODE_CLASS, CODE_END_CLASS, CODE_FOREIGN_CLASS, CODE_METHOD_INSTANCE, CODE_METHOD_STATIC, CODE_END_MODULE, CODE_IMPORT_MODULE, CODE_IMPORT_VARIABLE, CODE_END, }
 
 // Generate the stack effects table for opcodes
-mixin (() {
-    import std.conv : to;
-    string str = "static immutable stackEffects = [";
-    static foreach(op; WREN_OPCODE_FULL_TABLE) {
-        str ~= to!string(op.stackEffects) ~ ", ";
-    }
-    str ~= "];";
-    return str;
-}());
+// PERF: byte or int for interpreter perf?
+static immutable stackEffects = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, -1, -1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, 0, 0, -1, -1, -1, -1, 0, 1, 0, 0, -1, -2, -1, -2, -2, 1, 1, 1, 0, ];
